0e3fd86955e0c3867c1271f4c84c6ed956dc00ee
IamDevSteph <stephanie.ingraham3493@gmail.com>
2023-04-01 17:37:40 -0400

    version 1 symptoms class complete

--- src/main/java/NotaDoctor/NotADoctor/models/Symptoms.java
+++ src/main/java/NotaDoctor/NotADoctor/models/Symptoms.java
@@ -1,4 +1,29 @@
 package NotaDoctor.NotADoctor.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Symptoms {
+    @Id
+    @GeneratedValue
+    public Long id;
+    public String symptomsName;
+
+    public Symptoms(){
+
+    }
+
+    public Symptoms(Long id, String symptomsName){
+        this.id = id;
+        this.symptomsName = symptomsName;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public String getSymptomsName(){
+        return symptomsName;
+    }
 }
